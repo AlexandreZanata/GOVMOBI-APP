2e78a4f9ab9b6e564429b937e69769eba6686076
AlexandreZanata <zanataxandyzv07@gmail.com>
2026-04-15 07:32:50 -0400

    fix(expo): resolve runtime 'require' startup crash
    - switch Metro config to extend expo/metro-config
    
    - use babel-preset-expo in babel.config.js
    
    - restore root App.tsx to standard Expo default export
    
    - install expo-font and react-native-worklets peers
    
    - ignore .expo local state directory

--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,29 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-        alias: {
-          '@components': './src/components',
-          '@screens': './src/screens',
-          '@navigation': './src/navigation',
-          '@services': './src/services',
-          '@models': './src/models',
-          '@store': './src/store',
-          '@hooks': './src/hooks',
-          '@i18n': './src/i18n',
-          '@theme': './src/theme',
-          '@utils': './src/utils',
+module.exports = function (api) {
+  api.cache(true);
+
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@navigation': './src/navigation',
+            '@services': './src/services',
+            '@models': './src/models',
+            '@store': './src/store',
+            '@hooks': './src/hooks',
+            '@i18n': './src/i18n',
+            '@theme': './src/theme',
+            '@utils': './src/utils',
+          },
         },
-      },
+      ],
+      'react-native-reanimated/plugin',
     ],
-    'react-native-reanimated/plugin',
-  ],
+  };
 };
